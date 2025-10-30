1773ea7f6fca4caa69cae51f4775ad18ff692e04
Tavi <andrei230834@gmail.com>
2025-10-30 05:13:39 +0200

    ex1

---
+++ CBS.CBA.py
@@ -0,0 +1,12 @@
+import math
+import numpy as np
+from typing import Union
+
+def p1(a: str, p: float) -> Union[float, str]:
+    capacitate=0
+    if a=="CBS":
+        capacitate=1+ p*np.log2(p)+(1-p)*np.log2(1-p) if (p!=1 and p!=0) else 1
+    elif a=="CBA":
+        capacitate=1-p
+    else:
+        return "Date de intrare invalide!"
